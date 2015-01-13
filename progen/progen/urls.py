a4edf6d6a94a5fed27386d801b3b543ad86ff59b
deshraj <deshrajdry@gmail.com>
2015-01-13 06:19:03 +0530

    settings and urls configured

---
+++ progen/progen/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
+from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+# import settings
+# import allot.urls
+from allot.urls import * 
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'progen.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
