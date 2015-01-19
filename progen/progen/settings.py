2f64cc7f40b9c2bb3228af4166bfdd9186bd0bd8
deshraj <deshrajdry@gmail.com>
2015-01-19 13:45:13 +0530

    changes n templates of login and register D

--- progen/progen/settings.py
+++ progen/progen/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'progen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'progendb',
+        'USER':'root',
+        'PASSWORD':'helloworld',
     }
 }
 
